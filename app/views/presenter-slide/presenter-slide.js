7f7fc7d908aaa119cacc451332d2a64088764fda
Amir Mohsen Abdolrazaghi <ama56@kent.ac.uk>
2015-03-26 17:50:28 +0000

    fixes and enhancements - highlight.js

--- app/views/presenter-slide/presenter-slide.js
+++ app/views/presenter-slide/presenter-slide.js
@@ -2,5 +2,8 @@ Polymer({
 	domReady: function () {
 		$(this.$.slideContent).html($(this).html());
 		$(this).empty();
+		$(this.$.slideContent).find('pre code').each(function(i, block) {
+			hljs.highlightBlock(block);
+		});
 	}
 });
