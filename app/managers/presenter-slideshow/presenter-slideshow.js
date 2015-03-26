f0199bc7680434b2c39e3702b02451850c6ea6fd
Amir Mohsen Abdolrazaghi <amirmohsen.a@gmail.com>
2015-03-26 01:57:12 +0000

    bugfixes and css enhancements

--- app/managers/presenter-slideshow/presenter-slideshow.js
+++ app/managers/presenter-slideshow/presenter-slideshow.js
@@ -1,4 +1,5 @@
 Polymer({
+
 	created: function() {
 		this.metadata = {
 			title: "",
@@ -6,31 +7,40 @@ Polymer({
 		};
 		this.slides = [];
 	},
+
 	domReady: function() {
 		this.loadSlides();
 		this.watchHeight();
 	},
+
+	updateTitle: function() {
+		$("title").text(this.metadata.title);
+	},
+
 	watchHeight: function() {
 		this.$.frame.adjustHeight(this.topbarHeight());
 		this.$.sidebar.adjustHeight(this.topbarHeight());
 		var self = this;
 		$(window).on("resize", function(){
 			self.$.frame.adjustHeight(self.topbarHeight());
-			self.$.sidebar.adjustHeight(this.topbarHeight());
+			self.$.sidebar.adjustHeight(self.topbarHeight());
 		});
 	},
+
 	topbarHeight: function() {
 		return $(this.$.topbar).outerHeight();
 	},
+
 	loadSlides: function() {
 		var self = this;
-		$.getJSON("/slides/slides.json", function(json){
+		$.getJSON("./slides/slides.json", function(json){
 			self.metadata.title = json.title;
 			self.metadata.author = json.author;
 			self.slides = json.slides;
-			Polymer.import(["/slides/"+json.output], function(){
+			self.updateTitle();
+			Polymer.import(["./slides/"+json.output], function(){
 				var content = document
-					.querySelector('link[rel="import"][href="/slides/'+
+					.querySelector('link[rel="import"][href="./slides/'+
 						json.output+'"]').import;
 
 				var template = content.querySelector("template#slide-repo");
@@ -38,6 +48,14 @@ Polymer({
 				self.$.frame.setCurrentSlide(1);
 				self.$.sidebar.generateItems(template.content);
 			});
+			// self.addStyles(json.styles);
 		});
+	},
+
+	addStyles: function(styles) {
+		styles.forEach(function(style) {
+			var $link = $("<link>").attr({rel: "stylesheet", href: "./slides/"+style});
+			$("head").append($link);
+		});		
 	}
 });
--- gulp_tasks/concat_slides.js
+++ gulp_tasks/concat_slides.js
@@ -1,32 +1,43 @@
-var readJSON = require('readjson');
+var 
+	readJSON = require('readjson'),
+	vars = {
+		assets: "./slides/assets"
+	};
 
 function concat_slides() {
 
-	readJSON('./slides/slides.json', function(error, json) {
-		if (error)
-			console.error(error.message);
-		else if(json.slides)
-			injectFiles(json.slides);
-	});
+	concat();
 
-	function injectFiles(slides) {
+	gulp.watch("./slides/*.{html,json}", concat);
 
-		var slidesPaths = [];
+	function concat() {
 
-		slides.forEach(function(slideFileName){
-			slidesPaths.push("./slides/"+slideFileName+".html");
+		readJSON('./slides/slides.json', function(error, json) {
+			if (error)
+				console.error(error.message);
+			else if(json.slides)
+				injectFiles(json.slides);
 		});
 
-		var slidesStream = gulp.src(slidesPaths);
+		function injectFiles(slides) {
 
-		gulp.src("./src/all_slides.html")
-			.pipe($.inject(slidesStream,{
-				name: "slides",
-				transform: function (filePath, file) {
-					return file.contents.toString("utf8");
-				}
-			}))
-			.pipe(gulp.dest("./slides/concat/"))
+			var slidesPaths = [];
+
+			slides.forEach(function(slideFileName){
+				slidesPaths.push("./slides/"+slideFileName+".html");
+			});
+
+			var slidesStream = gulp.src(slidesPaths).pipe($.mustache(vars));
+
+			gulp.src("./src/all_slides.html")
+				.pipe($.inject(slidesStream,{
+					name: "slides",
+					transform: function (filePath, file) {
+						return file.contents.toString("utf8");
+					}
+				}))
+				.pipe(gulp.dest("./slides/concat/"));
+		}
 	}
 }
 
